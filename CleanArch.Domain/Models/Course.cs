f2344027524f85d2ac2ca0a361375d34c0e46765
shailendra.nigam <shailendra.nigam@gmail.com>
2026-06-15 13:21:52 +0530

    Add Course Domin Model

---
+++ CleanArch.Domain/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text;
+
+namespace CleanArch.Domain.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
