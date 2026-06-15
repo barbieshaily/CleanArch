855389f70422ae030dcd5d2305ac180506c63e5f
shailendra.nigam <shailendra.nigam@gmail.com>
2026-06-15 14:21:32 +0530

    Commit Service and Repository Interfaces, CourseViewModels

---
+++ CleanArch.Domain/Interfaces/ICourseRepository.cs
@@ -0,0 +1,12 @@
+﻿using CleanArch.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArch.Domain.Interfaces
+{
+    public interface ICourseRepository
+    {
+        IEnumerable<Course> GetCourses { get; set; }
+    }
+}
